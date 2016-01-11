cdce3d7b91c6c4d67248505018d872209f65e970
Lee Spendlove <lee.spendlove@gmail.com>
2016-01-11 18:02:26 +0000

    Add search bar jsx. Update config.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Config from '../config.js';
-
-const API_KEY = Config.youtube.key;
-
-console.log(API_KEY);
-
+import SearchBar from './components/search_bar';
+import { API_KEY } from '../config';
 
 // Create component that will produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Render component to the page/DOM
