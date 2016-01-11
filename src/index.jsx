57ba1b2df4aa43da026345fdc28f75fda8dc3551
Lee Spendlove <lee.spendlove@gmail.com>
2016-01-11 14:03:15 +0000

    Stubbed out component jsx file. Create config.js for API key adding to gitignore. Moved jsconfig.json to root

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Config from '../config.js';
+
+const API_KEY = Config.youtube.key;
+
+console.log(API_KEY);
+
+
 // Create component that will produce some HTML
 const App = () => {
   return <div>Hi!</div>;
